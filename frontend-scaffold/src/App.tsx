a9f11300af9c090bf85c132d0160b31133d72f0c
SamixYasuke <samueladekolu4@gmail.com>
2026-03-25 18:15:38 +0100

    feat: add husky hook

--- frontend-scaffold/src/App.tsx
+++ frontend-scaffold/src/App.tsx
@@ -11,8 +11,26 @@ import LandingPage from './features/landing/LandingPage';
 // import DashboardPage from './features/dashboard/DashboardPage';
 // import LeaderboardPage from './features/leaderboard/LeaderboardPage';
 
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col bg-white">
+        <Header />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            {/* Routes to be enabled as features are built:
+            <Route path="/@:username" element={<TipPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/leaderboard" element={<LeaderboardPage />} />
+            */}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
 
-export default App
+export default App;
